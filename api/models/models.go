0d0ef1d91898936e2497f39f960ee839a34f737f
poonsht <poon_arsene_lupin@hotmail.com>
2020-04-13 14:03:09 +0700

    [+] add : bson.id on model

--- api/models/models.go
+++ api/models/models.go
@@ -2,13 +2,14 @@ package models
 
 import (
 	"time"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Review struct {
-	// ID			primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ReviewID	bson.ObjectId	`bson:"_id"`
 	Text		string		`json:"text" bson:"text"`
-	// Author		string		`json:"author" bson:"author"`
-	// Grade		string		`json:"grade" bson:"grade"`
-	// Auth		string		`json:"auth" bson:"auth"`
+	Author		string		`json:"author" bson:"author"`
+	Grade		string		`json:"grade" bson:"grade"`
+	Auth		string		`json:"auth" bson:"auth"`
 	CreatedAt	time.Time	`json:"createdAt" bson:"created_at"`
 }
