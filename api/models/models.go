d29d446c0b89c6f25901725323e816c82dd6ecff
poonsht <poon_arsene_lupin@hotmail.com>
2020-04-13 14:35:57 +0700

    [-] fix: forgot rebuild on docker-compose

--- api/models/models.go
+++ api/models/models.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Review struct {
-	ReviewID	bson.ObjectId	`bson:"_id"`
-	Text		string		`json:"text" bson:"text"`
-	Author		string		`json:"author" bson:"author"`
-	Grade		string		`json:"grade" bson:"grade"`
-	Auth		string		`json:"auth" bson:"auth"`
-	CreatedAt	time.Time	`json:"createdAt" bson:"created_at"`
+	ReviewID	bson.ObjectId	`json:"review_id" bson:"_id"`
+	Text		string			`json:"text" bson:"text"`
+	Author		string			`json:"author" bson:"author"`
+	Grade		string			`json:"grade" bson:"grade"`
+	Auth		string			`json:"auth" bson:"auth"`
+	CreatedAt	time.Time		`json:"createdAt" bson:"created_at"`
 }
