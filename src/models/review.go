2a7fc8b4b9fa3732464930a435f7baddd28f3bbe
poonsht <poon_arsene_lupin@hotmail.com>
2020-11-10 17:16:26 +0700

    [+] add : sec, semester, year fields on schema

--- src/models/review.go
+++ src/models/review.go
@@ -6,6 +6,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// Review is model for create review
 type Review struct {
 	ID			bson.ObjectId	`json:"reviewId" bson:"_id"`
 	ClassID		string			`json:"classId" bson:"class_id"`
@@ -18,17 +19,22 @@ type Review struct {
 	Stats		StatReview		`json:"stats" bson:"stats"`	
 	ClassNameTH	string			`json:"classNameTH" bson:"class_name_th"`
 	ClassNameEN	string			`json:"classNameEN" bson:"class_name_en"`
+	Sec			uint64			`json:"sec" bson:"sec"`
+	Semester	uint64			`json:"semester" bson:"semester"`
+	Year		uint64			`json:"year" bson:"year"`
 	CreatedAt	time.Time		`json:"createdAt" bson:"created_at"`
 	UpdateAt	time.Time		`json:"updateAt" bson:"update_at"`
 	Reported	bool			`json:"reported" bson:"reported"`
 }
 
+// StatReview is model for storing stat on the review
 type StatReview struct {
 	How			float64		`json:"how" bson:"how"`
 	Homework	float64		`json:"homework" bson:"homework"`
 	Interest	float64		`json:"interest" bson:"interest"`
 }
 
+// ResReview is struct for body response to client 
 type ResReview struct {
 	ID			bson.ObjectId	`json:"reviewId" bson:"_id"`
 	ClassID		string			`json:"classId" bson:"class_id"`
@@ -39,14 +45,22 @@ type ResReview struct {
 	Boo			uint64			`json:"boo" bson:"boo"`
 	ClassNameTH	string			`json:"classNameTH" bson:"class_name_th"`
 	ClassNameEN	string			`json:"classNameEN" bson:"class_name_en"`
+	Sec			uint64			`json:"sec" bson:"sec"`
+	Semester	uint64			`json:"semester" bson:"semester"`
+	Year		uint64			`json:"year" bson:"year"`
 	CreatedAt	time.Time		`json:"createdAt" bson:"created_at"`
 	UpdateAt	time.Time		`json:"updateAt" bson:"update_at"`
 	Reported	bool			`json:"reported" bson:"reported"`
 }
 
+// RDeleteReview is require struct for delete the review
 type RDeleteReview struct {
 	ID			string			`json:"reviewId" bson:"_id"`
 	Auth		string			`json:"auth" bson:"auth"`
 }
 
 
+
+// Sec			uint64			`json:"sec" bson:"sec"`
+// Semester	uint64			`json:"semester" bson:"semester"`
+// Year		uint64			`json:"year" bson:"year"`
