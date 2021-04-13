e4d68bb7eeab551f22c0d7f31b7f4e4a6a67674d
aslupin <poon_arsene_lupin@hotmail.com>
2021-04-13 23:51:06 +0700

    [+] add : recap's DAO

---
+++ src/dao/recaps.go
@@ -0,0 +1,129 @@
+package dao
+
+import (
+	"log"
+	"strconv"
+
+	"kuclap-review-api/src/models"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// CreateRecap is POST create recap.
+func (m *SessionDAO) CreateRecap(recap models.Recap) error {
+	db	:=	session.Copy()
+	defer db.Close()
+
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Insert(&recap)
+	
+	return err
+}
+
+// UpdateNumberRecapByClassID is Update number of review
+func (m *SessionDAO) UpdateNumberDonwloadedByRecapID(recapID string) error {
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Update(bson.M{"_id": bson.ObjectIdHex(recapID)}, bson.M{ "$inc": bson.M{"downloaded": int32(1)} })
+	
+	return err
+}
+
+// UpdateNumberRecapByClassID is Update number of review
+func (m *SessionDAO) UpdateNumberRecapByClassID(classID string, number int) error {
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	err	:=	db.DB(m.Database).C(COLLECTION_CLASSES).Update(bson.M{"class_id": classID}, bson.M{ "$inc": bson.M{"number_recaps": int32(number)} })
+	
+	return err
+}
+
+// FindAllRecaps is Find All of list of recap 
+func (m *SessionDAO) FindAllRecaps() ([]models.Recap, error) {
+	var recaps []models.Recap
+
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Find(bson.M{}).All(&recaps)
+	return recaps, err
+}
+
+// FindRecapsByClassID is Find recap by class_id
+func (m *SessionDAO) FindRecapsByClassID(classID string, page string, offset string) ([]models.Recap, error) {
+	var recaps []models.Recap
+
+	db := session.Copy()
+	defer db.Close()
+
+	iPage, err		:=	strconv.Atoi(page)
+	if err != nil {
+		log.Println("err : atoi.", err)
+	}
+
+	iOffset, err	:=	strconv.Atoi(offset)
+	if err != nil {
+		log.Println("err : atoi.", err)
+	}
+
+	err = db.DB(m.Database).C(COLLECTION_RECAPS).Find(bson.M{"class_id": classID}).Sort("-$natural").Skip(iPage * iOffset).Limit(iOffset).All(&recaps)
+	
+	return recaps, err
+}
+
+// LastRecaps is Find last recaps range with offset
+func (m *SessionDAO) LastRecaps(page string, offset string) ([]models.Recap, error) {
+	var recaps []models.Recap
+
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	iPage, err		:=	strconv.Atoi(page)
+	if err != nil {
+		log.Println("err : atoi.", err)
+	}
+
+	iOffset, err	:=	strconv.Atoi(offset)
+	if err != nil {
+		log.Println("err : atoi.", err)
+	}
+
+	err	=	db.DB(m.Database).C(COLLECTION_RECAPS).Find(bson.M{}).Sort("-$natural").Skip(iPage * iOffset).Limit(iOffset).All(&recaps)
+	
+	return recaps, err
+}
+
+// FindRecapByID is Find recap by recap_id
+func (m *SessionDAO) FindRecapByID(recapID string) (models.Recap, error) {
+	var recap models.Recap
+
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Find(bson.M{"_id": bson.ObjectIdHex(recapID) }).One(&recap)
+	
+	return recap, err
+}
+
+// FindRecapAllPropertyByID is Find recap by recap_id
+func (m *SessionDAO) FindRecapAllPropertyByID(recapID string) (models.Recap, error) {
+	var recap models.Recap
+
+	db	:=	session.Copy()
+	defer db.Close()
+	
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Find(bson.M{"_id": bson.ObjectIdHex(recapID) }).One(&recap)
+	
+	return recap, err
+}
+
+// DeleteRecapByID is Delete an existing review
+func (m *SessionDAO) DeleteRecapByID(recapID string) error {
+	db	:=	session.Copy()
+	defer db.Close()
+
+	err	:=	db.DB(m.Database).C(COLLECTION_RECAPS).Remove(bson.M{"_id": bson.ObjectIdHex(recapID)})
+	
+	return err
+}
